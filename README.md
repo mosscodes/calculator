6a49fc99e469aa496e8ddf325bbb16c1e9dfa7f7
mosscodes <mossbusiness@outlook.com>
2021-02-27 21:01:01 -0500

    edit README.md, edit calculator.js, edit index.html, edit styles.css, create .gitignore

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ This Calculator project is part of the Odin Project curriculum
 The project primarily displays HTML, CSS, and Javascript skills
 
 The end result will be a HTML calculator styled with CSS and powered by Javascript code
+
+The calculator purposefully only evaluates two numbers at at time.
+
+I will admit, I had to throw out my code and start from scratch at one point. This was not easy.
--- calculator.js
+++ calculator.js
@@ -1,41 +1,225 @@
-//todo
-//clear (reset to zero)
-//negative positive switch
+const numberBtns = document.querySelectorAll('[data-number]');
+const operationBtns = document.querySelectorAll('[data-operation]');
+const deleteBtn = document.querySelector('[data-delete]');
+const clearBtn = document.querySelector('[data-clear]');
+const equateBtn = document.querySelector('[data-equate]');
+const topDisplay = document.querySelector('[data-top-display]');
+const bottomDisplay = document.querySelector('[data-bottom-display]');
 
-function add(num1, num2) {
-    if (num1 === "Error"){
-        num1 = 0;
-    }
-    return Math.round((num1 + num2) * 100000) / 100000;
+
+// function numPress(e) {
+//     const key = document.querySelector(`.num[data-key="${e.key}"]`);
+//     displayBottomNumber(key.dataset.number);
+// }
+// window.addEventListener('keydown', numPress);
+
+
+//currently not working
+//Purpose: press operator keys
+// function opPress(e) {
+//     const key = document.querySelector(`.op[data-key="${e.key}"]`);
+//     operation(key.dataset.operation);
+// }
+
+// window.addEventListener('keydown', opPress);
+
+
+let numContainer = "0";
+let num1 = null;
+let num2 = null;
+let operator = null;
+let firstOperator = null;
+let secondOperator = null;
+
+bottomDisplay.innerText = numContainer;
+
+//12 + 7 - 5 * 3 = 42
+
+for (let i = 0; i < operationBtns.length; i++) {
+    operationBtns[i].addEventListener('click', function() {
+
+
+        operator = this.dataset.operation;
+        setOperator(operator);
+        operation(operationBtns[i]);
+        updateTopDisplay();
+    })
 }
 
-function subtract(num1, num2) {
-    if (num1 === "Error"){
-        num1 = 0;
-    }
-    return Math.round((num1 - num2) * 100000) / 100000;
+for (let i = 0; i < numberBtns.length; i++) {
+    numberBtns[i].addEventListener('click', function() {
+        if (bottomDisplay.innerText === "Error") {
+            return;
+        }
+        displayBottomNumber(this.dataset.number);
+    })
 }
 
-function divide(num1, num2) {
-    if (num1 === "Error"){
-        num1 = 0;
+deleteBtn.addEventListener('click', function() {
+    undoNumber();
+});
+
+clearBtn.addEventListener('click', function() {
+    clearDisplay()
+});
+
+equateBtn.addEventListener('click', function() {
+    equate();
+});
+
+
+function displayBottomNumber(num) { 
+    if (numContainer.length > 26) {
+        return;
     }
-    if (num2 === 0) {
-        return "Error";
+    if (numContainer === "0" && num === '.' ) {
+        numContainer = "0.";
+        return bottomDisplay.innerText = numContainer;
+    }
+    if (numContainer === "0") {
+        numContainer = '';
+    }
+    if (numContainer.includes(".") === true && num === '.') {
+        return;
+    } 
+    numContainer = numContainer.concat(num);
+    return bottomDisplay.innerText = numContainer;
+}
+
+function undoNumber() {
+    if (bottomDisplay.innerText === "Error") {
+        return;
+    }
+    numContainer = numContainer.slice(0, -1);
+    if (numContainer.length === 0) {
+        numContainer = "0"
+        return bottomDisplay.innerText = numContainer;
+    }
+    return bottomDisplay.innerText = numContainer;
+}
+
+function updateTopDisplay() {
+    if (num1 !== null && firstOperator !== null && num2 !== null) {
+        topDisplay.innerText = `${num1} ${firstOperator} ${num2} =`
+    }
+    else if (num1 !== null && firstOperator !== null) {
+        topDisplay.innerText = `${num1} ${firstOperator}`
     }
     else {
-        return Math.round((num1 / num2) * 100000) / 100000;
+        topDisplay.innerText = '';
     }
 }
 
-function multiply(num1, num2){
-    if (num1 === "Error"){
-        num1 = 0;
-    } 
-    return Math.round((num1 * num2) * 100000) / 100000;
+function clearDisplay() {
+    numContainer = "0";
+    bottomDisplay.innerText = numContainer;
+    topDisplay.innerText = '';
+    operator = null;
+    firstOperator = null;
+    secondOperator = null;
+    num1 = null;
+    num2 = null;
 }
 
-function operate(operator, num1, num2){
-    displayNum = operator(num1, num2);
-    return displayNum;
+function setOperator(operator) {
+    if (firstOperator !== null && secondOperator !== null) {
+        firstOperator = null;
+        secondOperator = null;
+    }
+
+    if (firstOperator === null) {
+        return firstOperator = operator;
+    }
+    else {
+        return secondOperator = operator;
+    }
 }
+
+
+function operation() {
+    if (bottomDisplay.innerText === "Error") {
+        return;
+    }
+
+    if (secondOperator === null) { 
+        setValue(); 
+        return;
+    }
+    if (secondOperator !== null) {
+        setValue() 
+        updateTopDisplay()
+        const rawAnswer = operate(firstOperator, num1, num2);
+        if (rawAnswer === "Error") {
+            bottomDisplay.innerText = "Error";
+            return;
+        }
+        const answer = Math.round(rawAnswer * 1000000) / 1000000;
+        firstOperator = secondOperator;
+        secondOperator = null;
+        num1 = answer;
+        num2 = null;
+        bottomDisplay.innerText = answer;
+        numContainer = '0'
+        return;
+    }
+}
+
+function setValue() {
+    
+    if (num1 === null) {
+        
+        num1 = numContainer;
+        numContainer = "0";
+    }
+    else { 
+        
+        num2 = numContainer;
+        numContainer = "0";
+    }
+}
+
+function operate(operator, a, b) {
+    a = Number(a);
+    b = Number(b);
+    switch (operator) {
+        case "+":
+            return a + b;
+        case "-":
+            return a - b;
+        case "x":
+            return a * b;
+        case "÷":
+            if (b === 0) {
+                return "Error";
+            }
+            else {
+                return a / b;
+            }
+        default:
+            return null;
+  }
+}
+
+function equate() {
+    if (num1 === null) {
+        return;
+    }
+    setValue();
+    updateTopDisplay()
+    let rawAnswer = null;
+    let answer = null;
+    rawAnswer = operate(firstOperator, num1, num2);
+    if (rawAnswer === "Error") {
+        bottomDisplay.innerText = "Error";
+        return;
+    }
+    answer = Math.round(rawAnswer * 1000000) / 1000000;
+    bottomDisplay.innerText = answer;
+    num1 = answer;
+    num2 = null;
+    firstOperator = null;
+    secondOperator = null;
+}
+
+
+//it's setting b as zero because equate zeroed out num container in the lst one
